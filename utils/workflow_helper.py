7eff829110700d95c05515c5c38415e4c335ec50
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-24 10:26:28 +0700

    limit = 10

--- utils/workflow_helper.py
+++ utils/workflow_helper.py
@@ -106,7 +106,8 @@ def fetch_and_lock_pending_tasks(conn, step_code: str, limit: int = None) -> lis
         if env.CRAWL_DATA_ENV_DEV:
             limit = 2
         else:
-            limit = 100
+            limit = 10
+            # limit = 100
             # limit = None
 
     logger.info(f"Bắt đầu lấy và khóa task cho step_code='{step_code}', limit={limit}")
