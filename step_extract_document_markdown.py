6312141cff13d02924580ab1d85de03118f71c41
VuVanNghia <vuvannghia.work@gmail.com>
2026-03-24 20:48:57 +0700

    limit=1000,

--- step_extract_document_markdown.py
+++ step_extract_document_markdown.py
@@ -52,7 +52,7 @@ def document_markdown_resource(success_item_ids: list, error_item_ids: list):
         pending_item_ids = fetch_and_lock_pending_tasks(
             conn=conn,
             step_code=config_by_path.NAME,
-            limit=None,
+            limit=1000,
         )
 
         if not pending_item_ids:
