5372e439cd54a2246a0e8e5bc8d517de285b503b
VuVanNghia <vuvannghia.work@gmail.com>
2026-04-03 18:19:19 +0700

    500

--- crawler/spiders/document_detail_spider.py
+++ crawler/spiders/document_detail_spider.py
@@ -30,7 +30,8 @@ class DocumentDetailSpider(scrapy.Spider):
                 pending_item_ids = fetch_and_lock_pending_tasks(
                     conn=conn,
                     step_code="step_crawl_document_detail",
-                    limit=2 if env.CRAWL_DATA_ENV_DEV else 100,
+                    limit=2 if env.CRAWL_DATA_ENV_DEV else 500,
+                    # limit=2 if env.CRAWL_DATA_ENV_DEV else 1000,
                 )
         except Exception as e:
             logger.error(f"Lỗi khi lấy logic database từ PostgreSQL: {e}")
